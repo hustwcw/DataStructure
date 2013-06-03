22ee271e62fc36371594e19075f2cc8891d35dbc
Wang Chengwen <user@usertekiiMac.local>
2013-06-03 10:00:35 +0800

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Wang Chengwen on 13-6-3.
+//  Copyright (c) 2013年 user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
