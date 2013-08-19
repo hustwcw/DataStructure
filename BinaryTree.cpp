1656964131535147c5343e7c463d553440c2c652
Wang <wang@linuxmint.linuxmint-domain>
2013-08-19 10:46:55 +0800

    Construct a binarytree by level order traverse; determine if treeA has subtree of treeB

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -63,8 +63,61 @@ BinaryTreeNode *Construct(int *preorder, int *inorder, int length)
 }
 
 
+BinaryTreeNode *Construct(int *nodeArray, int length)
+{
+    if(nodeArray == NULL || length == 0)
+        return NULL;
+
+    queue<BinaryTreeNode *>nodeQueue;
+    BinaryTreeNode *head = new BinaryTreeNode;
+    head->value = nodeArray[0];
+    nodeQueue.push(head);
+    BinaryTreeNode *p = NULL;
+    int pos = 1;
+    while(!nodeQueue.empty())
+    {
+        if(pos+1 >= length)
+        {
+            cout << "input error" << endl;
+            return NULL;
+        }
+
+        p = nodeQueue.front();
+        nodeQueue.pop();
+        if(nodeArray[pos] > 0)
+        {
+            p->left = new BinaryTreeNode;
+            p->left->value = nodeArray[pos];
+            nodeQueue.push(p->left);
+        }
+        else
+        {
+            p->left = NULL;
+        }
+
+        if(nodeArray[pos+1] > 0)
+        {
+            p->right = new BinaryTreeNode;
+            p->right->value = nodeArray[pos+1];
+            nodeQueue.push(p->right);
+        }
+        else
+        {
+            p->right = NULL;
+        }
+        pos += 2;
+    }
+
+    return head;
+}
+
+
 void LevelOrder(BinaryTreeNode *root)
 {
+    if (root == NULL) {
+        return;
+    }
+
 	queue<BinaryTreeNode *> nodeQueue;
 	nodeQueue.push(root);
 	BinaryTreeNode *p = NULL;
@@ -99,15 +152,70 @@ BinaryTreeNode *mirror(BinaryTreeNode *head)
 
     return head;
 }
+
+
+bool HasRootSubTree(BinaryTreeNode *rootA, BinaryTreeNode *rootB)
+{
+    if (rootB == NULL) {
+        return true;
+    }
+    if (rootA == NULL)
+    {
+        return false;
+    }
+    else
+    {
+        if (rootA->value == rootB->value) {
+            return (HasRootSubTree(rootA->left, rootB->left) && HasRootSubTree(rootA->right, rootB->right));
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+bool HasSubTree(BinaryTreeNode *rootA, BinaryTreeNode *rootB)
+{
+    if (rootA == NULL || rootB == NULL) {
+        return false;
+    }
+
+    queue<BinaryTreeNode *> nodeQueue;
+    nodeQueue.push(rootA);
+    BinaryTreeNode *p = NULL;
+    while(!nodeQueue.empty())
+    {
+        p = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (HasRootSubTree(p, rootB)) {
+            return true;
+        }
+        if (p->left) {
+            nodeQueue.push(p->left);
+        }
+        if(p->right)
+        {
+            nodeQueue.push(p->right);
+        }
+
+    }
+    return false;
+}
+
+
 int main()
 {
 	int preorder[] = {1, 2, 4, 7, 3, 5, 6, 8};
 	int inorder[] = {4, 7, 2, 1, 5, 3, 8, 6};
+    int nodeArrayA[] = {8, 8, 7, 9, 2, 0, 0, 0, 0, 4, 7, 0, 0, 0, 0};
+    int nodeArrayB[] = {8, 9, 2, 0, 0, 4, 7, 0, 0, 0, 0};
 
-	BinaryTreeNode *root = Construct(preorder, inorder, 8);
-	LevelOrder(root);
+	BinaryTreeNode *rootA = Construct(nodeArrayA, sizeof(nodeArrayA)/sizeof(int));
+    BinaryTreeNode *rootB = Construct(nodeArrayB, sizeof(nodeArrayB)/sizeof(int));
 
-    LevelOrder(mirror(root));
-
+    bool result = HasSubTree(rootA, rootB);
+    cout << result << endl;
 	return 0;
 }
